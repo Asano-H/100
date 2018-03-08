09488c152a2469544400639204acee42c6a96cbb
Asano-H <31731068+Asano-H@users.noreply.github.com>
2018-03-08 22:29:57 +0900

    Delete 03.py

--- 03.py
+++
@@ -1,9 +0,0 @@
-text = "Now I need a drink, alcoholic of course, after the heavy lestures involving quantum mechanics."
-words = text.split(' ')
-result = []
-for word in words:
-    num = len(word)
-    count =(word.count(',') + word.count('.'))
-    result.append(num - count)
-
-print(result)
