2fc0f3338a8a7c28a13208088fb8fff1376adfb1
Asano-H <princessknk0406@gmail.com>
2018-03-22 00:20:36 +0900

    add code

--- C100/09.c
+++ C100/09.c
@@ -1,2 +1,19 @@
 /*整数値を入力させ、値が正であればpositive、負であればnegative、
 0であればzeroと表示するプログラムを作成せよ。*/
+
+#include<stdio.h>
+int main(void){
+	int num;
+	printf("input number: ");
+	scanf("%d",&num);
+
+	if(num==0){
+		printf("zero");
+	}else if(num>0){
+		printf("positive");
+	}else{
+		printf("negative");
+	}
+
+	return 0;
+}
