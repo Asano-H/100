2f48d3ac6b6014a4c1006aa98735f624009cee7d
Asano-H <princessknk0406@gmail.com>
2018-03-23 21:31:03 +0900

    add code

--- C100/10.c
+++ C100/10.c
@@ -1,1 +1,14 @@
 /*整数値を入力させ、その値を絶対値にして表示するプログラムを作成せよ。*/
+
+#include<stdio.h>
+int main(void){
+	int num;
+	printf("input number: ");
+	scanf("%d",&num);
+
+	if(num<0)
+		num=num*(-1);
+	printf("absolute value is %d",num);
+
+	return 0;
+}
