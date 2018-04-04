e30e3dcfe591e96208206f624c00eb12644ddb98
Asano-H <princessknk0406@gmail.com>
2018-04-04 22:47:13 +0900

    add comments

--- C100/21.c
+++ C100/21.c
@@ -1,9 +1,12 @@
+/*整数値を入力させ、その値が5よりも大きく、
+かつ、20よりも小さければOKと表示するプログラムを作成せよ。*/
+
 #include<stdio.h>
 int main(void){
 	int num;
 	printf("input number: ");
 	scanf("%d",&num);
-	
+
 	if(5<num && num<20){
 		printf("OK");
 	}
