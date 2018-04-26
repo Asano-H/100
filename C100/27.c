448aba4d4bc1750a3c672d3dc465c9d9e408492b
Asano-H <princessknk0406@gmail.com>
2018-04-26 23:52:13 +0900

    add code

--- C100/27.c
+++ C100/27.c
@@ -1,11 +1,18 @@
+/*整数値を入力させ、1からその値までの総和を計算して表示するプログラムを作成せよ。
+ただし、0以下の値を入力した場合は0と表示する。*/
+
 #include<stdio.h>
 int main(void){
-	int num,sum=0;
+	int i,num,sum=0;
 	printf("input number: ");
 	scanf("%d",&num);
 
-	for(int i=0;i<=num;i++){
-		sum = sum+i;
+	if(num<=0){
+		sum = 0;
+	}else{
+		for(i=0;i<=num;i++){
+			sum = sum+i;
+		}
 	}
 
 	printf("sum = %d",sum);
