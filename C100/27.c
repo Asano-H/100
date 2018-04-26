c73b0f3eb3db13ebe9507736141934f685287aac
Asano-H <princessknk0406@gmail.com>
2018-04-26 23:48:09 +0900

    commit

---
+++ C100/27.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+	int num,sum=0;
+	printf("input number: ");
+	scanf("%d",&num);
+
+	for(int i=0;i<num;i++){
+		sum = sum+i;
+	}
+
+	printf("sum = %d",sum);
+	return 0;
+}
